293ee3bf064a65a1ba2d4db801a690d20de8dfed
suyash-purwar <purwarsuyash06@gmail.com>
2026-02-21 12:45:28 +0530

    feat: add tokens for fonts and colors

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,4 +1,16 @@
+import { Raleway, Merriweather_Sans } from 'next/font/google';
+
 import "@/app/globals.css";
+
+const primaryFont = Raleway({
+  subsets: ['latin'],
+  variable: '--primary-font',
+});
+
+const secondaryFont = Merriweather_Sans({
+  subsets: ['latin'],
+  variable: '--secondary-font',
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${primaryFont.variable} ${secondaryFont.variable}`}>
       <body>
         {children}
       </body>
